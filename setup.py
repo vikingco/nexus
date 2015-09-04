f40af2383b08fb14b0cd887f7e6190fdf759268a
Adam Chainz <adam@adamj.eu>
2015-09-04 11:51:40 +0100

    YPlan version

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ class mytest(test):
 
 setup(
     name='nexus',
-    version='0.3.1',
+    version='0.3.1+yplan1',
     author='Disqus',
     author_email='opensource@disqus.com',
     url='https://github.com/disqus/nexus',
