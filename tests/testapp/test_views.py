1ef0507b90ee17b6d30717b1d481102ccba5ea0c
Adam Chainz <adam@adamj.eu>
2015-09-08 13:50:16 +0100

    Better testing of media views

--- tests/testapp/test_views.py
+++ tests/testapp/test_views.py
@@ -21,6 +21,7 @@ class ViewTests(TestCase):
         assert resp.status_code == 200
         print resp.content
         assert "Model Admin" in resp.content
+        assert 'csrftoken' in resp.cookies
 
     def test_logout_not_logged_in(self):
         self.client.logout()
@@ -36,3 +37,22 @@ class ViewTests(TestCase):
     def test_media_logo(self):
         resp = self.client.get('/nexus/media/nexus/img/nexus_logo.png')
         assert resp.status_code == 200
+        assert 'Last-Modified' in resp
+        assert 'Content-Length' in resp
+
+    def test_media_slash_slash_ignored(self):
+        resp = self.client.get('/nexus/media/nexus/img//nexus_logo.png')
+        assert resp.status_code == 200
+
+    def test_media_modified_since(self):
+        resp = self.client.get('/nexus/media/nexus/img/nexus_logo.png',
+                               HTTP_IF_MODIFIED_SINCE='Wed, 25 Feb 2065 17:42:04 GMT')
+        assert resp.status_code == 304
+
+    def test_media_non_existent(self):
+        resp = self.client.get('/nexus/media/nexus/img/doesnotexist.png')
+        assert resp.status_code == 404
+
+    def test_media_directory_not_allowed(self):
+        resp = self.client.get('/nexus/media/nexus/img/doesnotexist.png')
+        assert resp.status_code == 404
