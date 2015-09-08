62b02977fb92ef516c60127a9af59b236582ad69
Adam Chainz <adam@adamj.eu>
2015-09-08 14:20:23 +0100

    Test on Django 1.8

--- tests/settings.py
+++ tests/settings.py
@@ -48,7 +48,12 @@ TEMPLATES = [
         'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
-            'context_processors': [],
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
         },
     },
 ]
