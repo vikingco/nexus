bec0dcd9058dcb7344689b5479c509ef0693c88a
Adam Chainz <adam@adamj.eu>
2015-12-11 17:24:04 +0000

    Use Django's built-in autodiscover functionality
    It was factored out to make it reusable in Django ticket #21060: https://code.djangoproject.com/ticket/21060 , which is merged in all the Django versions we support, so we should use it.

--- nexus/__init__.py
+++ nexus/__init__.py
@@ -2,6 +2,8 @@
 Nexus
 ~~~~~
 """
+from django.utils.module_loading import autodiscover_modules
+
 from nexus.sites import NexusSite, site
 from nexus.modules import NexusModule
 
@@ -11,70 +13,6 @@ __all__ = ('autodiscover', 'NexusSite', 'NexusModule', 'site')
 
 default_app_config = 'nexus.apps.NexusAppConfig'
 
-# A flag to tell us if autodiscover is running.  autodiscover will set this to
-# True while running, and False when it finishes.
-LOADING = False
 
-# XXX: code based on django.contrib.admin auto discovery
-
-
-def autodiscover(site=None):
-    """
-    Auto-discover INSTALLED_APPS nexus.py modules and fail silently when
-    not present. This forces an import on them to register any api bits they
-    may want.
-
-    Specifying ``site`` will register all auto discovered modules with the new site.
-    """
-    # Bail out if autodiscover didn't finish loading from a previous call so
-    # that we avoid running autodiscover again when the URLconf is loaded by
-    # the exception handler to resolve the handler500 view.  This prevents an
-    # admin.py module with errors from re-registering models and raising a
-    # spurious AlreadyRegistered exception (see #8245).
-    global LOADING
-    if LOADING:
-        return
-    LOADING = True
-
-    if site:
-        orig_site = globals()['site']
-        globals()['site'] = locals()['site']
-
-    import imp
-    from importlib import import_module
-
-    from django.conf import settings
-
-    for app in settings.INSTALLED_APPS:
-        # For each app, we need to look for an api.py inside that app's
-        # package. We can't use os.path here -- recall that modules may be
-        # imported different ways (think zip files) -- so we need to get
-        # the app's __path__ and look for admin.py on that path.
-
-        # Step 1: find out the app's __path__ Import errors here will (and
-        # should) bubble up, but a missing __path__ (which is legal, but weird)
-        # fails silently -- apps that do weird things with __path__ might
-        # need to roll their own admin registration.
-        try:
-            app_path = import_module(app).__path__
-        except (AttributeError, ImportError):
-            continue
-
-        # Step 2: use imp.find_module to find the app's admin.py. For some
-        # reason imp.find_module raises ImportError if the app can't be found
-        # but doesn't actually try to import the module. So skip this app if
-        # its admin.py doesn't exist
-        try:
-            imp.find_module('nexus_modules', app_path)
-        except ImportError:
-            continue
-
-        # Step 3: import the app's admin file. If this has errors we want them
-        # to bubble up.
-        import_module("%s.nexus_modules" % app)
-
-    if site:
-        globals()['site'] = orig_site
-
-    # autodiscover was successful, reset loading flag.
-    LOADING = False
+def autodiscover():
+    autodiscover_modules('nexus_modules', register_to=site)
